e4e5c102c0574d9f6f94a0b45ebec1884567eb2e
aDENTinTIME <arixbizz@gmail.com>
2017-05-24 10:35:52 -0700

    Create Two_Blinks.ino

---
+++ Code/Two_Blinks.ino
@@ -0,0 +1,17 @@
+// Two_Blinks
+
+void setup() {
+  // initialize digital pins 11 & 12 as outputs.
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(11, HIGH);  //11 ON
+  delay(500);
+  digitalWrite(11, LOW);  //11 OFF
+
+  digitalWrite(12, HIGH);  //12 ON
+  delay(250);
+  digitalWrite(12, LOW);  //12 OFF
+}
